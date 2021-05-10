ec4c59aa19c83b3abe289423cc3f80c923028dd7
Saamnuur <inuur65@gmail.com>
2021-05-10 14:05:51 +0000

    nav bar updated on contact and england page, js page updated so nav bar works as a drop down, new js page created for the maps section, a litttle code was added to modify the maps

---
+++ assets/js/map.js
@@ -0,0 +1,11 @@
+//Maps pulling//
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+}
